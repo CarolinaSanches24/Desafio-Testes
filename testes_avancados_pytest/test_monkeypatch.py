cf40c4e0718f303679ea09e8641b385a3424991f
CarolinaSanches24 <carolmosilva34@gmail.com>
2024-03-05 20:48:10 -0300

    :sparkles: feat: adicionando resolução test_monkeypatch.py

--- testes_avancados_pytest/test_monkeypatch.py
+++ testes_avancados_pytest/test_monkeypatch.py
@@ -1,1 +1,28 @@
 #Aplicar patch e substituir
+import pytest
+import os
+
+
+def test_os(monkeypatch):
+    #permite substituir a funcao os.path.exists pela função lambda
+    monkeypatch.setattr('os.path.exists', lambda x: False)
+    assert os.path.exists('/') is False
+    
+class SomarNumeros:
+    def add(self,a,b):
+        return a+b
+    
+def test_somar_numeros(monkeypatch):
+    calc = SomarNumeros();
+    
+    def custom_add(self,a,b):
+        return a*b
+    
+    monkeypatch.setattr(SomarNumeros,'add',custom_add)
+    
+    assert calc.add(2,3)==6
+    
+    # Restaurando o método original após o teste
+    monkeypatch.undo()
+    
+    assert calc.add(2,3)==5 
