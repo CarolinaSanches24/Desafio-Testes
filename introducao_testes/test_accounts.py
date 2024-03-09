83195781e3b1282fd135e35767189bfe562fa7e9
janessf <janesilva0128@gmail.com>
2024-03-09 20:19:05 -0300

    Adicionando resolucao do test_accounts.py

--- introducao_testes/test_accounts.py
+++ introducao_testes/test_accounts.py
@@ -1,1 +1,22 @@
-# Convenções de Nomenclatura
+#Convenções de nomenclatua
+import unittest
+
+class Account: 
+    def create(self):                                    #criar uma conta:
+       return True
+    
+    def delete(self):                                    # excluir conta
+        return True
+
+class TestAccounts(unittest.TestCase):
+    def test_creation(self):
+        account = Account()
+        self.assertTrue(account.create())
+
+    def test_deletion(self):
+        account = Account()
+        self.assertTrue(account.delete())
+
+if __name__ == '__main__':
+    unittest.main()
+
