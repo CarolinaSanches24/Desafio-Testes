fcf12a6c588aed7278f1348c6871706a2ae95ba9
SimoneBraghin <braghin.aai@gmail.com>
2024-03-09 15:39:30 -0300

    adição de atividade concluída.

--- fazer_testes_pytest/tests/test_main.py
+++ fazer_testes_pytest/tests/test_main.py
@@ -1,7 +1,25 @@
+import pytest
 # Testar funções
+def test_main():
+    assert "a string value" == "a string value"
 
+# Função default
+def default():
+    return "default username"
 
 #Classes de teste e métodos de teste
+class TestUser:
 
+    def test_username(self):
+        assert default() == "default username"
 
 # Executar testes
+        
+if __name__ == "__main__":
+    import pytest
+    pytest.main()
+        
+# contents of test_main.py file
+
+def test_main():
+    assert True
