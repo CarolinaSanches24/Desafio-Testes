28c9b928d0d104481dc301bc0f42d2bcd7aecadb
maiharamoni <maiharamoni@gmail.com>
2024-03-10 19:50:44 -0300

    resolucao dos exercicios Introducao a testes no Python Desafio 05

--- introducao_testes/test_exercise.py
+++ introducao_testes/test_exercise.py
@@ -5,3 +5,27 @@
 # Corrigir o bug e fazer com que os testes passem
 
 # Adicionar novo código com testes
+
+import unittest
+
+def str_to_bool(value):
+    true_values = ['y','yes']
+    false_values = ['no', 'n']
+
+    if value in true_values:
+        return True
+    if value in false_values:
+        return False
+    
+class TestStrToBool(unittest.TestCase):
+
+    def test_y_is_true(self):
+        result = str_to_bool('y')
+        self.assertTrue(result)
+
+    def test_yes_is_true(self):
+        result = str_to_bool('Yes')
+        self.assertTrue(result)
+
+if __name__ == '__main__':
+    unittest.main()
