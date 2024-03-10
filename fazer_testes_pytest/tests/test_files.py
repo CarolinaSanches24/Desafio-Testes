46109aa0831d3266ad6e846357c03c9ce214bdd2
SimoneBraghin <braghin.aai@gmail.com>
2024-03-10 11:28:21 -0300

    adição de arquivo teste e alteração no readme

--- fazer_testes_pytest/tests/test_files.py
+++ fazer_testes_pytest/tests/test_files.py
@@ -1,10 +1,34 @@
-# Criar uma classe de teste
+import os
+import tempfile
+import pytest
 
-# Métodos auxiliares
+def is_done(path):
+    if not os.path.exists(path):
+        return False
+    with open(path) as _f:
+        contents = _f.read()
+    if "yes" in contents.lower():
+        return True
+    elif "no" in contents.lower():
+        return False
+    
+class TestIsDone:
+    @classmethod
+    def setup_class(cls):
+        cls.tmp_dir = tempfile.mkdtemp()
+        cls.tmp_file = os.path.join(cls.tmp_dir, "test_file")
 
-#Limpeza
+    @classmethod
+    def teardown_class(cls):
+        os.remove(cls.tmp_file)
+        os.rmdir(cls.tmp_dir)
 
-# Instalação
+    def test_yes(self):
+        with open(self.tmp_file, "w") as _f:
+            _f.write("yes")
+        assert is_done(self.tmp_file) is True
 
-
-# Métodos auxiliares personalizados
+    def test_no(self):
+        with open(self.tmp_file, "w") as _f:
+            _f.write("no")
+        assert is_done(self.tmp_file) is False
