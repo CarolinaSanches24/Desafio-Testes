229c19842ed57b663abb1fa97a84877294182d6b
janessf <janesilva0128@gmail.com>
2024-03-10 14:34:08 -0300

    Adicionando test_args_params.py

--- testes_avancados_pytest/test_args_params.py
+++ testes_avancados_pytest/test_args_params.py
@@ -1,1 +1,8 @@
 # Usar vários argumentos
+
+#Teste para verificar se um objeto tem um atributo,com o uso da função interna hasattr() do Python
+import pytest
+
+@pytest.mark.parametrize("item, attribute", [("", "format"), (list(), "append")])
+def test_attributes(item, attribute):
+    assert hasattr(item, attribute)
